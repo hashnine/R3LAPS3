f49ce559834921e93ce0dab689911fd78c57e098
sinsly <126437023+hashnine@users.noreply.github.com>
2024-05-01 00:54:19 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1215,7 +1215,7 @@ local function getObjGen()
             Gui.Watermark.TextStrokeTransparency = 0.800
             Gui.Watermark.TextXAlignment = Enum.TextXAlignment.Left
             Gui.Watermark.Position = UDim2.new(0, 0, 0, 0)
-            Gui.Watermark.Transparency = 1
+            Gui.Watermark.Visible = false
 
             Gui.UIPadding_6.Parent = Gui.Window
             Gui.UIPadding_6.PaddingBottom = UDim.new(0, 8)
