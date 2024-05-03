00fe584e6621fbe350db7981a5bda62cb1409040
sinsly <126437023+hashnine@users.noreply.github.com>
2024-05-03 11:08:47 -0400

    Create icons.lua

---
+++ Relaptic/icons.lua
@@ -0,0 +1,22 @@
+local categoryicons = {
+    loader = "http://www.roblox.com/asset/?id=4458808375",
+    player = "http://www.roblox.com/asset/?id=8395621517",
+    eye = "http://www.roblox.com/asset/?id=17345858628",
+    world = "http://www.roblox.com/asset/?id=11395780588",
+    sword = "http://www.roblox.com/asset/?id=7485051715",
+    holder = "http://www.roblox.com/asset/?id=15662114094",
+    settings = "http://www.roblox.com/asset/?id=13568966069",
+    list = "http://www.roblox.com/asset/?id=5107183916",
+    mastery = "http://www.roblox.com/asset/?id=17345971019",
+    chat = "http://www.roblox.com/asset/?id=11702915127",
+    target = "http://www.roblox.com/asset/?id=17346002479",
+    servers = "http://www.roblox.com/asset/?id=17346014007",
+    teleport = "http://www.roblox.com/asset/?id=17346032895",
+    folder = "http://www.roblox.com/asset/?id=17346048800",
+    save = "http://www.roblox.com/asset/?id=17346051574",
+    key = "http://www.roblox.com/asset/?id=17346052540",
+    shield = "http://www.roblox.com/asset/?id=17346054236",
+    farm = "http://www.roblox.com/asset/?id=17346066763",
+}
+
+local ci = categoryicons
