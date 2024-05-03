7d0f9703bbfac9ed83809668888976cf2e85724a
sinsly <126437023+hashnine@users.noreply.github.com>
2024-05-03 11:27:57 -0400

    Update loader.lua

--- Relapse/loader.lua
+++ Relapse/loader.lua
@@ -1,1 +1,11 @@
--- will finish loadstring setup
+local Place = game.PlaceId
+local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/hashnine/R3LAPS3/main/Relaptic/notification.lua"))()
+if Place == 914010731 then
+    Notify.New("[Successfully] - Game supported : Ro-Ghoul", 2)
+  -- loadstring placing soon
+elseif
+Place == 111111111 then
+    Notify.New("[Successfully] - Game supported : Template", 2)
+else
+    Notify.New("[Error] - Game not supported.", 2)
+end
