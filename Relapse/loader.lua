b40378eac6e880e4f7e88afd3b636d55c5d274be
sinsly <126437023+hashnine@users.noreply.github.com>
2024-05-03 21:29:17 -0400

    Update loader.lua

--- Relapse/loader.lua
+++ Relapse/loader.lua
@@ -2,10 +2,13 @@ local Place = game.PlaceId
 local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/hashnine/R3LAPS3/main/Relaptic/notification.lua"))()
 if Place == 914010731 then
     Notify.New("[Successfully] - Game supported : Ro-Ghoul", 2)
-  -- loadstring placing soon
+  wait(1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hashnine/R3LAPS3/main/Relapse/List/roghoul.lua"))()
 elseif
-Place == 111111111 then
-    Notify.New("[Successfully] - Game supported : Template", 2)
+Place == 6549794549 or 6678600773 or 13272886093 or 7235817949 or 8026790371 then
+    Notify.New("[Successfully] - Game supported : RH2", 2)
+    wait(1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hashnine/R3LAPS3/main/Relapse/List/rh2.lua"))()
 else
     Notify.New("[Error] - Game not supported.", 2)
 end
